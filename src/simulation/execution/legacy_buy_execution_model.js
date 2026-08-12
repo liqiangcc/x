3225a7299e9e617641a54f6bc1227331be9f310a
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-12 16:13:29 +0800

    refactor(simulation): source legacy profile from catalog

--- src/simulation/execution/legacy_buy_execution_model.js
+++ src/simulation/execution/legacy_buy_execution_model.js
@@ -1,34 +1,28 @@
 "use strict";
 
 const {
+  LEGACY_A_SHARE_EXECUTION_PROFILE,
+} = require("./execution_profile_catalog");
+const {
   createProfiledBuyExecutionModel,
   normalizeProfileExecutionConfig,
   normalizeTickSize,
 } = require("./profiled_buy_execution_model");
 const { nonNegativeMoney } = require("./execution_model_support");
 
-const LEGACY_A_SHARE_BUY_PROFILE = Object.freeze({
-  kind: "legacy_a_share_next_open",
-  ruleApproximation: "legacy_rules_current_defaults",
-  tickSize: 0.01,
-  executionDefaults: Object.freeze({
-    lotSize: 100,
-    tPlusOne: true,
-  }),
-  qualityIssues: Object.freeze([]),
-});
+const LEGACY_A_SHARE_BUY_PROFILE = LEGACY_A_SHARE_EXECUTION_PROFILE;
 
 function normalizeExecutionConfig(input = {}) {
   return normalizeProfileExecutionConfig({
     input,
-    profile: LEGACY_A_SHARE_BUY_PROFILE,
+    profile: LEGACY_A_SHARE_EXECUTION_PROFILE,
   });
 }
 
 function createLegacyBuyExecutionModel({ executionConfig = {} } = {}) {
   return createProfiledBuyExecutionModel({
     executionConfig,
-    profile: LEGACY_A_SHARE_BUY_PROFILE,
+    profile: LEGACY_A_SHARE_EXECUTION_PROFILE,
   });
 }
 
