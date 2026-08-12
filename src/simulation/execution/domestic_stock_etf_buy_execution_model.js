65854043e41e307a075f76136279b3d168945697
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-12 16:13:44 +0800

    refactor(simulation): source ETF profile from catalog

--- src/simulation/execution/domestic_stock_etf_buy_execution_model.js
+++ src/simulation/execution/domestic_stock_etf_buy_execution_model.js
@@ -1,26 +1,16 @@
 "use strict";
 
+const {
+  DOMESTIC_STOCK_ETF_EXECUTION_PROFILE,
+} = require("./execution_profile_catalog");
 const { createProfiledBuyExecutionModel } = require("./profiled_buy_execution_model");
 
-const DOMESTIC_STOCK_ETF_BUY_PROFILE = Object.freeze({
-  kind: "domestic_stock_etf_next_open",
-  ruleApproximation: "domestic_stock_etf_current_approximation",
-  tickSize: 0.001,
-  executionDefaults: Object.freeze({
-    lotSize: 100,
-    tPlusOne: true,
-    stampDutyRate: 0,
-  }),
-  qualityIssues: Object.freeze([
-    "etf_profile_assumes_domestic_stock_etf_t_plus_one",
-    "etf_profile_does_not_cover_t_plus_zero_etf_categories",
-  ]),
-});
+const DOMESTIC_STOCK_ETF_BUY_PROFILE = DOMESTIC_STOCK_ETF_EXECUTION_PROFILE;
 
 function createDomesticStockEtfBuyExecutionModel({ executionConfig = {} } = {}) {
   return createProfiledBuyExecutionModel({
     executionConfig,
-    profile: DOMESTIC_STOCK_ETF_BUY_PROFILE,
+    profile: DOMESTIC_STOCK_ETF_EXECUTION_PROFILE,
   });
 }
 
