ca5d1209fc66ea5225a8413003f91e6420df7886
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-15 03:29:27 +0800

    refactor(cli): add shared option parser

---
+++ src/adapters/cli/options.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const DEFAULT_BOOLEAN_OPTIONS = Object.freeze(new Set([
+  "latest",
+  "commit",
+  "force",
+  "forcePool",
+  "forceUniverse",
+  "forceStrategyCodes",
+  "strategyOnly",
+  "allCodes",
+  "json",
+  "allowPartial",
+  "proxyPreflight",
+  "noProxyPreflight",
+]));
+
+function parseCliOptions(argv, { defaults = {}, booleanOptions = DEFAULT_BOOLEAN_OPTIONS } = {}) {
+  const options = { _: [], ...defaults };
+  for (let index = 0; index < argv.length; index += 1) {
+    const arg = argv[index];
+    if (!arg.startsWith("--")) {
+      options._.push(arg);
+      continue;
+    }
+
+    const key = arg.slice(2).replace(/-([a-z])/g, (_, char) => char.toUpperCase());
+    if (booleanOptions.has(key)) {
+      options[key] = true;
+      continue;
+    }
+
+    const nextArg = argv[index + 1];
+    if (!nextArg) {
+      throw new Error(`Missing value for ${arg}`);
+    }
+    options[key] = nextArg;
+    index += 1;
+  }
+  return options;
+}
+
+module.exports = {
+  DEFAULT_BOOLEAN_OPTIONS,
+  parseCliOptions,
+};
